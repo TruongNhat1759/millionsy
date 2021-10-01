5e4034ef4f3abd4255d1f191833a455bfb7135d5
nhattv <nhattv@mor.com.vn>
2021-10-01 13:33:25 +0700

    add source

--- src/components/pages/lottery/getSection/index.tsx
+++ src/components/pages/lottery/getSection/index.tsx
@@ -5,33 +5,33 @@ const GetSection: React.FC = () => {
   const DataItemCards = [
     {
       title: 'Get the tickets',
-      content: 'Every ticket equals to 2$ and can be bought with $SOL token. Price will be set when the round begins.',
+      content: ['Every ticket equals to 2$ and can be bought with',<br className="sp-768"/>,' $SOL token. Price will be set when the round begins.'],
       has_arrow: false,
     },
     {
       title: 'Pick your card',
-      content: 'Freely choose 6 different numbers from 1 to 45 the order doesn\'t matter). You can select by yourself or let the machine choose randomly.',
+      content: ['Freely choose 6 different numbers from 1 to 45 (the',<br className="sp-768"/>,'order doesn\'t matter). You can select by yourself or',<br className="sp-768"/>,' let the machine choose randomly.'],
       has_arrow: false,
     },
     {
       title: 'Wait for the party',
-      content: 'There is 1 draw every day, starts at 11am UTC and ends 5 minutes before the next round begins.',
+      content: ['There is 1 draw every day, starts at 11am UTC and',<br className="sp-768"/>,' ends 5 minutes before the next round begins.'],
       has_arrow: false,
     },
     {
       title: 'Reward',
-      content: 'Once the draw\'s out, come back to see if you\'re the lucky winner! The prize is automatically sent to the winner\'s wallet.',
+      content: ['Once the draw\'s out, come back to see if you\'re the',<br className="sp-768"/>,' lucky winner! The prize is automatically sent to the',<br className="sp-768"/>,' winner\'s wallet.'],
       has_arrow: false,
     },
     {
       title: 'Pop the champagne',
-      content: 'Match all 6 digits from the winning numbers and you win the jackpot. Or just match at least 3 to win a SHARED PRIZE.',
+      content: ['Match all 6 digits from the winning numbers and you',<br className="sp-768"/>,' win the jackpot. Or just match at least 3 to win a',<br className="sp-768"/>,' SHARED PRIZE.'],
       has_arrow: true,
       link: 'https://docs.millionsy.io/lottery/how-to-play'
     },
     {
       title: 'Fund?',
-      content: 'After every round, the $SOL in the unclaimed brackets will be rolled over into the next round and redistributed among the PRIZE POOL.',
+      content: ['After every round, the $SOL in the unclaimed',<br className="sp-768"/>,' brackets will be rolled over into the next round and',<br className="sp-768"/>,' redistributed among the PRIZE POOL.'],
       has_arrow: true,
       link: 'https://docs.millionsy.io/lottery/prize-pool'
     },
