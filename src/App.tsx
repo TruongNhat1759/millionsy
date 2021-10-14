48ce6e492b76e98948c6ff36b7372b6a83ca8a59
nhattv <nhattv@mor.com.vn>
2021-10-14 22:52:58 +0700

    add loadding

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import routeElements from './router/routeElements';
 
 const App: React.FC = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={
+      <div className='fixed h-100vh w-full top-0 left-0 z-100 '>
+        <div className='h-full absolute w-full top-0 left-0 bg-gray-400'></div>
+        <p className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-full z-1000 max-w-110'><img src="/assets/common/loading.gif" alt="loading" /></p>
+      </div>
+    }>
       <Router>
         <Switch>
           {routeElements()}
